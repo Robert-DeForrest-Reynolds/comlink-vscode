a633da224960e8ac2502e12dcd49188902c04efa
Robert DeForrest Reynolds <rldr.europa@gmail.com>
2025-09-27 12:42:33 -0700

    Add com-link init command and activation event
    Introduces a new 'com-link.init' command to initialize a 'com-link' directory in the workspace. Updates activation events to trigger when a workspace contains a 'com-link' file, and refactors extension activation logic to support the new command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let is_activated = false;
+
+
+function log(msg:string, pop:boolean=false){
+	console.log(msg);
+	if (pop){
+		vscode.window.showInformationMessage(msg);
+	}
+}
+
+function error(msg:string, pop:boolean=false){
+	console.error(msg);
+	if (pop){
+		vscode.window.showErrorMessage(msg);
+	}
+}
+
+
+async function init_com_link() {
+	if (is_activated) {
+		error("com-link is already activated", true);
+		return;
+	}
+	const workspace_folders = vscode.workspace.workspaceFolders;
+
+	if (!workspace_folders || workspace_folders.length === 0){
+		error("No workspace is open.", true);
+		return;
+	}
+	
+	if (workspace_folders.length === 2){
+		error("Detected 2 workspaces open, will only init for the first workspace detected.", true);
+	}
+
+	const workspace_uri = workspace_folders[0].uri;
+	const com_link_dir_path = vscode.Uri.joinPath(workspace_uri, 'com-link');
+
+
+	try {
+		const com_link_dir_stat = await vscode.workspace.fs.stat(com_link_dir_path);
+		if (com_link_dir_stat.type === vscode.FileType.Directory){
+			error('com-link directory already exists in workspace folder.', true);
+		} else {
+			log('Creating com-link directory...', true);
+			await vscode.workspace.fs.createDirectory(com_link_dir_path);
+			log(`Directory ready:\n${com_link_dir_path.fsPath}`, true);
+		}
+	} catch (err) {
+		error("Failed to create com-link directory for some reason.", true);
+	}
+}
+
+
 export function activate(context: vscode.ExtensionContext) {
+    const output = vscode.window.createOutputChannel('com-link');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "com-link" is now active!');
+	const init_command = vscode.commands.registerCommand('com-link.init', init_com_link);
+	context.subscriptions.push(init_command);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('com-link.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from com-link!');
-	});
-
-	context.subscriptions.push(disposable);
+	vscode.window.showInformationMessage("com-link active");
+	is_activated = true;
 }
 
 // This method is called when your extension is deactivated
